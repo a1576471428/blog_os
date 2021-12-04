4813f4d0a534de9b683f87877e3c590b76cc52bd
xiaochao <13942156310@163.com>
2021-12-04 23:56:58 +0800

    hello-world

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# os follow https://os.phil-opp.com/
+
+
+## run
+
+cargo xrun
+
+显示结果需要X-SERVER  
+wsl2下可以搞一个xterm把display设置为主机的ip:0.0就可以显示了
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 #![no_std]
 #![no_main]
 
-fn main() {}
-
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    print("Hello World!");
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+fn print(str: &str) {
+    let vga_buffer_start = 0xb8000 as *mut u8;
+    for (index, byte) in str.as_bytes().iter().enumerate() {
+        unsafe {
+            // content
+            *vga_buffer_start.offset(index as isize * 2) = *byte;
+            // color
+            *vga_buffer_start.offset(index as isize * 2 + 1) = 0xb;
+        }
+    }
 }
